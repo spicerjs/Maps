29688a2adb2f28ae94c759e1c5fc2b00d13db3e3
spicerjs <spicerjs@hotmail.com>
2020-08-02 04:54:40 +0200

    Add files via upload

--- teamratings.py
+++ teamratings.py
@@ -12,10 +12,8 @@ import base64
 import numpy as np
 import plotly as pl
 import plotly.graph_objs as gobj
-import pandas as pd
 from plotly.offline import download_plotlyjs,init_notebook_mode,plot,iplot
 init_notebook_mode(connected=True)
-
 
 st.title('Team Ratings')
 
@@ -31,9 +29,9 @@ st.sidebar.header('Teams and leagues to include')
 dfmap = df[['Country', 'Position']]
 dfmap.Details = np.where(dfmap.Position == 1, 1, 2)
 dfmap.Details = np.where(dfmap.Position == '', 3, dfmap.Details)
-dfmap.Country = np.where(dfmap.Country == 'Columbia', 'Colombia', dfmap.Country)
+#dfmap.Country = np.where(dfmap.Country == 'Columbia', 'Colombia', dfmap.Country)
 dfmap.Country = np.where(dfmap.Country == 'England', 'uk', dfmap.Country)
-dfmap.Country = np.where(dfmap.Country == 'Nth America', 'USA', dfmap.Country)
+#dfmap.Country = np.where(dfmap.Country == 'Nth America', 'USA', dfmap.Country)
 dfmap.Country = np.where(dfmap.Country == 'S Africa', 'South Africa', dfmap.Country)
 
 
@@ -57,27 +55,40 @@ iplot(col_map)
 st.sidebar.plotly_chart(col_map, use_container_width=True)
 
 
-all_tiers_option = ['Yes', 'No']
-tiers_option = st.sidebar.selectbox('Only include top division from each country?', all_tiers_option)
-if tiers_option == 'Yes':
+DEFAULT_CTRY = df[df.Rating == df.Rating.max()]['Country'].values[0]
+
+tiers_option = st.sidebar.checkbox('Exclude lower tier leagues', value=False) 
+if tiers_option:
     df = df[df.Tier == 1]
-#    df = df[df.TeamWiki.str.contains("xx") == False]
 df = df.sort_values(by='Rating', ascending=False)
 df = df.reset_index(drop=True) 
 df.index = df.index+1
 
-search_team_option = ['Yes','No']
-team_search = st.sidebar.selectbox('Search Team?', search_team_option)
-if team_search == 'Yes':
+team_search = st.sidebar.checkbox('Only show teams from chosen country', value=False) 
+if team_search:
     sorted_unique_country = sorted(df.Country.unique()) 
-    selected_country = st.sidebar.selectbox('Country', sorted_unique_country)
-    df1 = df[df.Country == selected_country]
-    sorted_unique_league = sorted(df1.League.unique())
+    DEFAULT = sorted_unique_country.index(DEFAULT_CTRY)
+    selected_country = st.sidebar.selectbox('Country', sorted_unique_country, index = DEFAULT)
+    df = df[df.Country == selected_country]
+    unique_league = sorted(df.League.unique())
+    selected_leagues = st.sidebar.multiselect('Leagues to include', unique_league, unique_league)
+    df = df[df.League.isin(selected_leagues)]#
+    df.Rating = df.Rating.map('{:.3f}'.format)
+    st.dataframe(df[['Country', 'League', 'Team', 'Rating']])
+
+else:
+    sorted_unique_country = sorted(df.Country.unique()) 
+    DEFAULT = sorted_unique_country.index(DEFAULT_CTRY)
+    selected_country = st.sidebar.selectbox('Country', sorted_unique_country, index = DEFAULT)
+    df1 = df[df.Country == selected_country].sort_values(by='Tier', ascending=True)
+    sorted_unique_league = df1.League.unique()
     if tiers_option != 'Yes':
         selected_league = st.sidebar.selectbox('League', sorted_unique_league)
         df1 = df1[df1.League == selected_league]
     sorted_unique_team = sorted(df1.Team.unique())
-    selected_team = st.sidebar.selectbox('Team', sorted_unique_team)
+    TOP_TEAM = df1[df1.Rating == df1.Rating.max()]['Team'].values[0]
+    DEFAULT_TEAM = sorted_unique_team.index(TOP_TEAM)
+    selected_team = st.sidebar.selectbox('Team', sorted_unique_team, index=DEFAULT_TEAM)
     df.Ranking = df.index.values
     ranking = df.Ranking[df.Team == selected_team].sum()-6
     ranking = max(0,ranking)
@@ -87,23 +98,10 @@ if team_search == 'Yes':
     st.dataframe(df[['Country', 'League', 'Team', 'Rating']].iloc[ranking:ranking+11])
     
 
-else:
-    all_countries = st.sidebar.selectbox('All countries?', search_team_option)
-    if all_countries == 'No':
-        sorted_unique_country = sorted(df.Country.unique()) 
-        selected_country = st.sidebar.selectbox('Country', sorted_unique_country)
-        df = df[df.Country == selected_country]
-        unique_league = sorted(df.League.unique())
-        selected_leagues = st.sidebar.multiselect('Leagues to include', unique_league, unique_league)
-        df = df[df.League.isin(selected_leagues)]
- 
-#    st.header('Team Ratings')
-    df.Rating = df.Rating.map('{:.3f}'.format)
-    st.dataframe(df[['Country', 'League', 'Team', 'Rating']])
-
-  
-st.markdown("""
-
-""")
-
+st.write()
+st.write(f"[Expected league finishes](https://world-league-predict.herokuapp.com#).")
+st.write()
+st.write(f"[Big games this coming week](https://sixpointers.herokuapp.com#).")
+st.write()         
+st.write(f"[https://elosports.wordpress.com/about/](https://elosports.wordpress.com/about/#).")
 
